5758e872be85ff59945458fe649d4343ed54f14d
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2021-05-25 11:57:34 +0700

    Init student screens

---
+++ source/src/main/java/com/lnh/CourseRegistration/UIs/Screens/RegistrationScreen/StudentRegisterResult.java
@@ -0,0 +1,4 @@
+package com.lnh.CourseRegistration.UIs.Screens.RegistrationScreen;
+
+public class StudentRegisterResult {
+}
---
+++ source/src/main/java/com/lnh/CourseRegistration/UIs/Screens/RegistrationScreen/StudentRegisterScreen.java
@@ -0,0 +1,14 @@
+package com.lnh.CourseRegistration.UIs.Screens.RegistrationScreen;
+
+import javax.swing.*;
+
+public class StudentRegisterScreen {
+    private JPanel mainPanel;
+    private JScrollPane paneRegister;
+    private JTable tableRegister;
+    private JButton btnRegister;
+    private JScrollPane paneRegistered;
+    private JTable tableRegistered;
+    private JButton btnCancelled;
+    private JLabel lblLeft;
+}
