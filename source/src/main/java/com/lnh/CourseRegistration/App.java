62f69d717082585e53396cc6ea31af372dca953e
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2021-05-26 11:04:07 +0700

    Add FlatLaF dependency for better UI looks

--- source/src/main/java/com/lnh/CourseRegistration/App.java
+++ source/src/main/java/com/lnh/CourseRegistration/App.java
@@ -1,24 +1,19 @@
 package com.lnh.CourseRegistration;
 
-import com.lnh.CourseRegistration.DAOs.RegistrationInfoDAO;
-import com.lnh.CourseRegistration.Entities.RegistrationInfo;
+import com.formdev.flatlaf.FlatLightLaf;
 import com.lnh.CourseRegistration.UIs.FormLogin;
 
 import javax.swing.*;
-import java.util.List;
 
 public class App {
     public static final int MAX_REGISTERED_COURSE = 8;
 
     public static void main(String[] args) {
-//        try {
-//            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-//        } catch (
-//                ClassNotFoundException | InstantiationException
-//                        | IllegalAccessException | UnsupportedLookAndFeelException e
-//        ) {
-//            e.printStackTrace();
-//        }
+        try {
+            UIManager.setLookAndFeel( new FlatLightLaf() );
+        } catch( Exception ex ) {
+            System.err.println( "Failed to initialize LaF" );
+        }
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
